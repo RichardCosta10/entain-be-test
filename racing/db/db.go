cf99839802c2c95e3bbae553c5e5053791cffdf3
Richard Costa <r.a.costa1988@gmail.com>
2023-06-27 10:47:09 +1000

    Add new status column to races table

--- racing/db/db.go
+++ racing/db/db.go
@@ -7,13 +7,15 @@ import (
 )
 
 func (r *racesRepo) seed() error {
-	statement, err := r.db.Prepare(`CREATE TABLE IF NOT EXISTS races (id INTEGER PRIMARY KEY, meeting_id INTEGER, name TEXT, number INTEGER, visible INTEGER, advertised_start_time DATETIME)`)
+	r.db.Exec(`DROP TABLE IF EXISTS races`)
+	statement, err := r.db.Prepare(`CREATE TABLE IF NOT EXISTS races (id INTEGER PRIMARY KEY, meeting_id INTEGER, name TEXT, number INTEGER, visible INTEGER, advertised_start_time DATETIME, status TEXT)`)
 	if err == nil {
 		_, err = statement.Exec()
 	}
 
 	for i := 1; i <= 100; i++ {
-		statement, err = r.db.Prepare(`INSERT OR IGNORE INTO races(id, meeting_id, name, number, visible, advertised_start_time) VALUES (?,?,?,?,?,?)`)
+		startTime := faker.Time().Between(time.Now().AddDate(0, 0, -1), time.Now().AddDate(0, 0, 2))
+		statement, err = r.db.Prepare(`INSERT OR IGNORE INTO races(id, meeting_id, name, number, visible, advertised_start_time, status) VALUES (?,?,?,?,?,?,?)`)
 		if err == nil {
 			_, err = statement.Exec(
 				i,
@@ -21,7 +23,14 @@ func (r *racesRepo) seed() error {
 				faker.Team().Name(),
 				faker.Number().Between(1, 12),
 				faker.Number().Between(0, 1),
-				faker.Time().Between(time.Now().AddDate(0, 0, -1), time.Now().AddDate(0, 0, 2)).Format(time.RFC3339),
+				startTime.Format(time.RFC3339),
+				func(startTime time.Time) string {
+					if startTime.Before(time.Now()) {
+						return "closed"
+					} else {
+						return "open"
+					}
+				}(startTime),
 			)
 		}
 	}
